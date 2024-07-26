d918306c6c2846c36c581bf7cb2124aba023fc68
Joao Donaton <joao.vitor.donaton@gmail.com>
2024-07-25 21:16:58 -0300

    navbar and page swapper

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// add listeners to clickable navbar items
+let navbarItems = document.getElementsByClassName("navbar-item");
+for(let item of navbarItems){
+    item.addEventListener("click", (e) => {
+        // change navbar highlight
+        let currentPageNav = document.getElementsByClassName("navbar-item-current")[0];
+        currentPageNav.classList.remove("navbar-item-current");
+
+        e.currentTarget.classList.add("navbar-item-current");
+
+        // change visible page
+        let currentPageContainer = document.getElementsByClassName("page-container-current")[0];
+        currentPageContainer.classList.remove("page-container-current");
+
+        let pageId = e.currentTarget.id;
+        pageId = pageId.replace("-item", "-page");
+
+        console.log(pageId)
+
+        document.getElementById(pageId).classList.add("page-container-current");
+    })
+}
